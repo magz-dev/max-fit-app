4c5b19c51a87a56b6319e79dda638bb4e9fa2921
magzdev <magdakloskowicz@gmail.com>
2023-11-11 18:28:00 +0000

    Update models.py

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
         
     name = models.CharField(max_length=100)
-    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+    friendly_name = models.CharField(max_length=100, null=True, blank=False)
 
     def __str__(self):
         return self.name
@@ -18,13 +18,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=False, on_delete=models.SET_NULL)
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=False)
+    image = models.ImageField(null=True, blank=False)
 
     def __str__(self):
         return self.name
