589579b4feb0d27933f6d4f114ec80ef1ff747f7
magzdev <magdakloskowicz@gmail.com>
2023-11-17 16:40:55 +0000

    Add python code to create 'add to bag' functionality

--- bag/views.py
+++ bag/views.py
@@ -7,3 +7,19 @@ def view_bag(request):
 
     return render (request, 'bag/shopping_bag.html')
 
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity # update item quantity
+    else:
+        bag[item_id] = quantity # add item quantity
+
+    request.session['bag'] = bag # overrides variable with an updated version
+    print(request.session['bag'])
+    return redirect(redirect_url)
+    
