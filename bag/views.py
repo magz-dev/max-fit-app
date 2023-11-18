2404ec9355faa959caadd7a9e539a95694026001
magzdev <magdakloskowicz@gmail.com>
2023-11-18 17:01:46 +0000

    Add code comments to the shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 def view_bag(request):
     """ A view to return the bag contents page """
 
-    return render (request, 'bag/shopping_bag.html')
+    return render (request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
