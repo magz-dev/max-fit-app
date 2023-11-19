94ce4cb1af476f594cb052efd80022625a9fb7c5
magzdev <magdakloskowicz@gmail.com>
2023-11-19 19:59:13 +0000

    Fix: Change navigation menu to display products according to gender

--- products/models.py
+++ products/models.py
@@ -20,11 +20,12 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=False, on_delete=models.SET_NULL)
     name = models.CharField(max_length=50)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=False)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
-    rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=False)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=False)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=False)
     image_url = models.URLField(max_length=1000, null=True, blank=False)
     image = models.ImageField(null=True, blank=False)
+
 
     def __str__(self):
         return self.name
