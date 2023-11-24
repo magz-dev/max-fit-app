7f81bb3f7dbffa810bfccbc6ce386b41248f5f45
magzdev <magdakloskowicz@gmail.com>
2023-11-24 18:12:12 +0000

    feat: update allauth templates

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,5 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
@@ -6,26 +7,28 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=15, null=False, editable=False)
-    full_name = models.CharField(max_length=30, null=False, blank=False)
-    email = models.EmailField(max_length=100, null=False, blank=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = CountryField(blank_label='Country *', null=False, blank=False)
-    postcode = models.CharField(max_length=30, null=True, blank=True)
-    town_or_city = models.CharField(max_length=30, null=False, blank=False)
-    street_address1 = models.CharField(max_length=50, null=False, blank=False)
-    street_address2 = models.CharField(max_length=50, null=True, blank=True)
-    county = models.CharField(max_length=30, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=200, null=False, blank=False, default='')
-
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -38,8 +41,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        lineitem_total_sum = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        self.order_total = lineitem_total_sum or 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -75,4 +77,5 @@ class OrderLineItem(models.Model):
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-  
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
