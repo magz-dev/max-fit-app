b9fe535c84ef522c78a7f27a8e4af69c17856c6e
magzdev <magdakloskowicz@gmail.com>
2023-11-24 22:17:21 +0000

    feat: add product admin form

---
+++ products/forms.py
@@ -0,0 +1,29 @@
+# Import necessary modules from Django
+from django import forms
+from .models import Product, Category
+
+# Create a form for handling Product model data
+class ProductForm(forms.ModelForm):
+
+    # Meta class to specify the model and fields for the form
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    # Constructor to initialize the form with additional customizations
+    def __init__(self, *args, **kwargs):
+        # Call the parent class constructor
+        super().__init__(*args, **kwargs)
+
+        # Retrieve all Category objects from the database
+        categories = Category.objects.all()
+
+        # Create a list of tuples containing category IDs and their friendly names
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        # Set the choices for the 'category' field to the list of friendly names
+        self.fields['category'].choices = friendly_names
+
+        # Add custom attributes to form fields for styling
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
