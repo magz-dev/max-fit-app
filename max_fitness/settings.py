0236d3bb27034373da2b11580609c4e84b519c9c
magzdev <magdakloskowicz@gmail.com>
2023-11-26 20:57:54 +0000

    fix: change secret key

--- max_fitness/settings.py
+++ max_fitness/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-d25414ap#6rr*&^s#nwjx-7v6_$uta_0^amn@(-ro@q(zm9ic1')
+SECRET_KEY = 'django-insecure-d25414ap#6rr*&^s#nwjx-7v6_$uta_0^amn@(-ro@q(zm9ic1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
