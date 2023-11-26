5cebd02984d0da977e55f926f1088f4750f7c1c1
magzdev <magdakloskowicz@gmail.com>
2023-11-26 19:21:15 +0000

    set debug to true

--- max_fitness/settings.py
+++ max_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-d25414ap#6rr*&^s#nwjx-7v6_$uta_0^amn@(-ro@q(zm9ic1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-magzdev-sportstylehub-u80esv7qb0e.ws-eu106.gitpod.io']
 
