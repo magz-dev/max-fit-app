a90d2a7d03e4a311b50b1f20028b7bf27cd7883a
magzdev <magdakloskowicz@gmail.com>
2023-12-26 22:35:35 +0000

    fix: delete code that is not needed

--- bag/admin.py
+++ bag/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
-from .models import Coupon
 
-
-# Coupon model in admin
-class CouponAdmin(admin.ModelAdmin):
-    list_display = ['code', 'amount']
-    list_filter = ['code', 'amount']
-    search_fields = ['code']
-
-
-admin.site.register(Coupon, CouponAdmin)
