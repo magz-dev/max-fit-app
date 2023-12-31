5e9db656c8a00e6a637044d1cc61b6e337326a2c
magzdev <magdakloskowicz@gmail.com>
2023-12-31 17:32:49 +0000

    feat: add 404 error template

--- max_fitness/urls.py
+++ max_fitness/urls.py
@@ -27,3 +27,9 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+
