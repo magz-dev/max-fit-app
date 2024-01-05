514dd9247dab1e81d800c74d87e43b11e1d24989
magzdev <magdakloskowicz@gmail.com>
2024-01-05 12:30:49 +0000

    feat: add view for 404 error page

--- max_fitness/urls.py
+++ max_fitness/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -30,7 +31,7 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-
+hendler404 = 'max_fitness.views.handler404'
 
 
 
