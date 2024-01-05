e152865354a3862df55d61546aef708f62a65e96
magzdev <magdakloskowicz@gmail.com>
2024-01-05 13:27:26 +0000

    fix: 404 error page

--- max_fitness/views.py
+++ max_fitness/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def handler404(request, exception):
-    """ Error handler 404 - Page not found """
-    return render(request, "404.html", status=404)
+    # 404 error handler
+    return render(request, 'errors/404.html', status=404)
+
+
+def handler500(request):
+    # 500 error handler
+    return render(request, 'errors/500.html', status=500)
